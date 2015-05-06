da592595fc057e35dbcdc08ae459005b561d101b
cleri philiastides & Jim Patt <student@epicodus.com>
2015-05-06 09:54:26 -0700

    Add and pass specs for #title, #author and #id

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,4 +9,16 @@ describe(Book) do
       expect(test_book.title()).to(eq("Welcome to the neighborhood"))
     end
   end
+  describe('#author') do
+    it('returns the author of the book')do
+      test_book = Book.new({:title => "Welcome to the neighborhood", :author => "Mr. Rogers", :id => nil})
+      expect(test_book.author()).to(eq("Mr. Rogers"))
+    end
+  end
+  describe('#id') do
+    it('returns the id of the book')do
+      test_book = Book.new({:title => "Welcome to the neighborhood", :author => "Mr. Rogers", :id => 1})
+      expect(test_book.id()).to(eq(1))
+    end
+  end
 end
